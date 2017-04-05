c9265807eda72982afdab45f840f3681e0976df4
LHS Compsci <lhscompsci@lh-l703-15-7368.ci.lexington.ma.us>
2017-04-05 12:40:35 -0400

    set up threads and keyListeners, ready to write gui

--- src/com/company/SnakeRunner.java
+++ src/com/company/SnakeRunner.java
@@ -1,11 +1,74 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 /**
  * Created by lhscompsci on 4/3/17.
  */
-public class SnakeRunner implements Runnable {
-    @Override public void run() {}
 
-    
+public class SnakeRunner{
+    static Snake python;
+    static char dir = 'R';
+    static int key;
+    public static void main(String Args[]) {
+        python = new Snake('R');
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        frame.getContentPane().add(panel);
+
+        panel.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                key = e.getKeyCode();
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {}
+        });
+        //make a gameboard
+
+        Thread snakeRunner = new Thread(new Runnable() {
+            private boolean running = true;
+            public void stopInTheNameOfLove() {
+                running = false;
+            }
+            @Override
+            public void run() {
+                while(running) {
+                    while (python.isAlive()) {
+                        switch(key) {
+                            case KeyEvent.VK_LEFT:
+                                dir = 'L';
+                                break;
+                            case KeyEvent.VK_RIGHT:
+                                dir = 'R';
+                                break;
+                            case KeyEvent.VK_DOWN:
+                                dir = 'D';
+                                break;
+                            case KeyEvent.VK_UP:
+                                dir = 'U';
+                                break;
+                        }
+                        python.changeDir(dir);
+                        python.updateSnake();
+                        if (!python.isAlive()) {
+                            stopInTheNameOfLove();
+                        }
+                        //gui stuff
+                    }
+                    //more gui stuff
+                }
+            }
+        }, "snakeRunner");
+        snakeRunner.start();
+    }
+
 
 }
