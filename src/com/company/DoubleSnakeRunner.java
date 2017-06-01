00cd2dbd2d0749c196ffb71e2a2e58582c2aa8d3
LHS Compsci <lhscompsci@lh-l703-15-7368.ci.lexington.ma.us>
2017-06-01 09:28:04 -0400

    added  second thread, untested

--- src/com/company/DoubleSnakeRunner.java
+++ src/com/company/DoubleSnakeRunner.java
@@ -9,7 +9,7 @@ import javax.swing.*;
 public class DoubleSnakeRunner extends JPanel {
     static Snake python, blackMamba;
     static Gameboard board;
-//    static Food food = null; //MOVE FOOD TO RUNNER AND OUT OF SNAKE? would have to be accesible from snake somehow
+    //    static Food food = null; //MOVE FOOD TO RUNNER AND OUT OF SNAKE? would have to be accesible from snake somehow
     static DoubleGui gui;
     private static boolean won = false;
     static int boardWidth, boardHeight;
@@ -19,8 +19,133 @@ public class DoubleSnakeRunner extends JPanel {
         python = new Snake('D', board, 'M');
         blackMamba = new Snake('U', board, 'M', 37, 37);
         gui = new DoubleGui();
-        blackMamba = new Snake('U', board, 'M', (gui.getWidth()/15)-1, (gui.getHeight()/15)-1);
-        Thread snakeRunner = new Thread(new Runnable() {
+        blackMamba = new Snake('U', board, 'M', (gui.getWidth() / 15) - 1, (gui.getHeight() / 15) - 1);
+        Thread snakeOneRunner = new Thread(new Runnable() {
+            private boolean running = true;
+
+            @Override
+            public void run() {
+                while (running) {
+                    gui.start = false;
+                    while (python.isAlive() && blackMamba.isAlive()) {
+                        boardWidth = gui.getWidth() / 15;
+                        boardHeight = gui.getHeight() / 15;
+
+                        board.setWidth(boardWidth);
+                        board.setHeight(boardHeight);
+
+                        if (boardWidth * boardHeight < python.getLength()) {
+                            gui.labelMessage(4);
+                            won = true;
+                            break;
+                        }
+                        if (!gui.pause) {
+                            gui.labelMessage(2); // NO TEXT
+
+                            python.dir = gui.getLeftDir(); // GET NEW DIR
+//                            blackMamba.dir = gui.getRightDir();
+
+                            python.changeDir(python.dir); // UPDATE SNAKE DIR
+//                            blackMamba.changeDir(blackMamba.dir);
+
+                            python.updateSnake(); // UPDATE SNAKE
+
+//                            blackMamba.setFood(python.getFood());
+//                            blackMamba.updateSnake();
+
+                            if (python.eaten) {
+                                blackMamba.setFood(python.getFood());
+                            } else if (blackMamba.eaten) {
+                                python.setFood(blackMamba.getFood());
+                            }
+
+
+                            gui.repaint(); // REDRAW GUI
+                            if (!python.isAlive() && blackMamba.isAlive()) {
+                                gui.start = false;
+                            }
+                        } else {
+                            gui.labelMessage(1); // PAUSE MESSAGE
+                            try {
+                                Thread.sleep(10);
+                            } catch (InterruptedException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    }
+                    if (!won) {
+                        if (python.getLength() > blackMamba.getLength()) { // P1 WINS
+                            gui.labelMessage(3);
+                        } else if (python.getLength() == blackMamba.getLength()) { // TIE
+                            gui.labelMessage(4);
+                        } else { // P2 WINS
+                            gui.labelMessage(5);
+                        }
+
+                        while (!gui.start) { // WAIT FOR RESTART
+                            try {
+                                Thread.sleep(10);
+                            } catch (InterruptedException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                        gui.resetKeyboardActions(); // RESET KEYBOARD INPUT
+                        python = new Snake('D', board, 'M'); // NEW SNAKE;
+                        blackMamba = new Snake('U', board, 'M', boardWidth - 1, boardHeight - 1);
+                        gui.labelMessage(0); // PRESS PLAY TO START MESSAGE
+
+                        gui.repaint(); // DRAW NEW SNAKE
+
+                        gui.start = false;
+
+                        while (!gui.start) { // WAIT FOR START
+                            try {
+                                Thread.sleep(10);
+                            } catch (InterruptedException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                        python.setSpeed(gui.speed);
+                        blackMamba.setSpeed(gui.speed);
+                        gui.labelMessage(2); // NO TEXT
+                    } else { // GAME WON
+                        won = false;
+
+                        gui.start = false;
+                        while (!gui.start) { // WAIT FOR RESTART
+                            try {
+                                Thread.sleep(10);
+                            } catch (InterruptedException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                        gui.labelMessage(0); // PRESS PLAY TO START MESSAGE
+                        gui.resetKeyboardActions(); // RESET KEYBOARD INPUT
+                        python = new Snake('D', board, 'M'); // NEW SNAKE
+                        blackMamba = new Snake('U', board, 'M', boardWidth - 1, boardHeight - 1);
+
+                        gui.repaint(); // DRAW NEW SNAKE
+
+                        gui.start = false;
+
+                        while (!gui.start) { // WAIT FOR START
+                            try {
+                                Thread.sleep(10);
+                            } catch (InterruptedException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                        python.setSpeed(gui.speed);
+                        gui.labelMessage(2); // NO TEXT
+                    }
+                }
+
+                //display lost message + score + hit space to try again and to reset snake /field
+                //more gui stuff
+//                }
+            }
+        }, "snakeRunner");
+        Thread snakeTwoRunner = new Thread(new Runnable() {
             private boolean running = true;
 
             @Override
@@ -76,7 +201,7 @@ public class DoubleSnakeRunner extends JPanel {
                     if (!won) {
                         if (python.getLength() > blackMamba.getLength()) { // P1 WINS
                             gui.labelMessage(3);
-                        } else if(python.getLength() == blackMamba.getLength()) { // TIE
+                        } else if (python.getLength() == blackMamba.getLength()) { // TIE
                             gui.labelMessage(4);
                         } else { // P2 WINS
                             gui.labelMessage(5);
@@ -91,7 +216,7 @@ public class DoubleSnakeRunner extends JPanel {
                         }
                         gui.resetKeyboardActions(); // RESET KEYBOARD INPUT
                         python = new Snake('D', board, 'M'); // NEW SNAKE;
-                        blackMamba = new Snake('U', board, 'M', boardWidth-1, boardHeight-1);
+                        blackMamba = new Snake('U', board, 'M', boardWidth - 1, boardHeight - 1);
                         gui.labelMessage(0); // PRESS PLAY TO START MESSAGE
 
                         gui.repaint(); // DRAW NEW SNAKE
@@ -108,8 +233,7 @@ public class DoubleSnakeRunner extends JPanel {
                         python.setSpeed(gui.speed);
                         blackMamba.setSpeed(gui.speed);
                         gui.labelMessage(2); // NO TEXT
-                    }
-                    else { // GAME WON
+                    } else { // GAME WON
                         won = false;
 
                         gui.start = false;
@@ -123,7 +247,7 @@ public class DoubleSnakeRunner extends JPanel {
                         gui.labelMessage(0); // PRESS PLAY TO START MESSAGE
                         gui.resetKeyboardActions(); // RESET KEYBOARD INPUT
                         python = new Snake('D', board, 'M'); // NEW SNAKE
-                        blackMamba = new Snake('U', board, 'M', boardWidth-1, boardHeight-1);
+                        blackMamba = new Snake('U', board, 'M', boardWidth - 1, boardHeight - 1);
 
                         gui.repaint(); // DRAW NEW SNAKE
 
@@ -140,12 +264,9 @@ public class DoubleSnakeRunner extends JPanel {
                         gui.labelMessage(2); // NO TEXT
                     }
                 }
-
-                //display lost message + score + hit space to try again and to reset snake /field
-                //more gui stuff
-//                }
             }
-        }, "snakeRunner");
+        }, "snakeRunnerTwo");
+
         gui.labelMessage(0);
         while (!gui.start) {
             try {
@@ -155,7 +276,9 @@ public class DoubleSnakeRunner extends JPanel {
             }
         }
         python.setSpeed(gui.speed);
+        blackMamba.setSpeed(gui.speed);
         gui.labelMessage(2);
-        snakeRunner.start();
+        snakeOneRunner.start();
+        snakeTwoRunner.start();
     }
 }
